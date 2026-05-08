b22f0beb85c61b9cf1a26ef82068016c79179379
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2026-05-08 08:05:11 +0800

    Update FirestoreService.cs

--- expo_Software_and_Site/app_desktop/comprehensure/DataBaseControl/firestore_setup/FirestoreService.cs
+++ expo_Software_and_Site/app_desktop/comprehensure/DataBaseControl/firestore_setup/FirestoreService.cs
@@ -1,12 +1,1 @@
-﻿/*using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace comprehensure.DataBaseControl.Services
-{
- public class FirestoreService
- {
-    
-}
-*/
+﻿
